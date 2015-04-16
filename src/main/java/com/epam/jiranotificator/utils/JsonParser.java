65e0a631786921b71c91c445ac2f8632b8597a1e
Marian_Basarab <Marian_Basarab@epbyminw0825.minsk.epam.com>
2015-04-16 17:06:36 +0300

    add iteration

--- src/main/java/com/epam/jiranotificator/utils/JsonParser.java
+++ src/main/java/com/epam/jiranotificator/utils/JsonParser.java
@@ -8,30 +8,30 @@ import org.json.simple.JSONObject;
 import com.epam.jiranotificator.dto.IssueDTO;
 
 public class JsonParser {
-	
+
 	private IssueDTO issueDTO;
 	private JSONArray issues;
 	private JSONObject fields;
-	
-	public IssueDTO readJson (JSONObject jsonFromJira) {
+
+	public IssueDTO readJson(JSONObject jsonFromJira) {
 		issueDTO = new IssueDTO();
-		
+
 		if (!jsonFromJira.isEmpty()) {
 			issues = (JSONArray) jsonFromJira.get("issues");
-			
+
 			Iterator<JSONObject> iterator = issues.iterator();
 			while (iterator.hasNext()) {
-				
-			JSONObject issue = (JSONObject) iterator.next(); 
-						
-			fields = (JSONObject) issue.get("fields");
-			JSONObject priority = (JSONObject) fields.get("priority");
 
-			issueDTO.setCode((String) issue.get("key"));
-			issueDTO.setStatus((String) priority.get("name"));
-			issueDTO.setDescription((String) fields.get("description"));
-			
-			return issueDTO;
+				JSONObject issue = iterator.next();
+
+				fields = (JSONObject) issue.get("fields");
+				JSONObject priority = (JSONObject) fields.get("priority");
+
+				issueDTO.setCode((String) issue.get("key"));
+				issueDTO.setStatus((String) priority.get("name"));
+				issueDTO.setDescription((String) fields.get("description"));
+
+				return issueDTO;
 			}
 		}
 		return null;
