6dd4184c4db8c1162ef67ebe479e10ba55e8564d
Marian_Basarab <Marian_Basarab@epbyminw0825.minsk.epam.com>
2015-04-16 16:56:36 +0300

    add IssueDTO and JsonParser

---
+++ src/main/java/com/epam/jiranotificator/utils/JsonParser.java
@@ -0,0 +1,33 @@
+package com.epam.jiranotificator.utils;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.epam.jiranotificator.dto.IssueDTO;
+
+public class JsonParser {
+	
+	private IssueDTO issueDTO;
+	private JSONArray issues;
+	private JSONObject fields;
+	
+	public IssueDTO readJson (JSONObject jsonFromJira) {
+		issueDTO = new IssueDTO();
+		
+		if (!jsonFromJira.isEmpty()) {
+			issues = (JSONArray) jsonFromJira.get("issues");
+			
+			JSONObject issue = (JSONObject) issues.get(0); 
+						
+			fields = (JSONObject) issue.get("fields");
+			JSONObject priority = (JSONObject) fields.get("priority");
+
+			issueDTO.setCode((String) issue.get("key"));
+			issueDTO.setStatus((String) priority.get("name"));
+			issueDTO.setDescription((String) fields.get("description"));
+			
+			return issueDTO;
+		}
+		return null;
+	}
+}
