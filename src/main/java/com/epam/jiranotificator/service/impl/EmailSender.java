2dfffd8ca2444526b66539337697104a91f54d7e
Bohdan Kolesnyk <bkolesnyk85@gmail.com>
2015-05-07 00:02:23 +0300

    Issue#8 refactor for using Spring email

--- src/main/java/com/epam/jiranotificator/service/impl/EmailSender.java
+++ src/main/java/com/epam/jiranotificator/service/impl/EmailSender.java
@@ -1,20 +1,10 @@
 package com.epam.jiranotificator.service.impl;
 
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
-
-import com.epam.jiranotificator.exception.EmailSenderException;
 
 /**
  * Service which send emails to specific address
@@ -28,52 +18,36 @@ public class EmailSender {
     private static final Logger LOG = LoggerFactory
             .getLogger(EmailSender.class);
 
+    private MailSender mailSender;
     private String emailFrom;
-    private String emailTo;
-    private String emailLogin;
-    private String emailPassword;
-
-    public EmailSender(String emailFrom, String emailTo, String emailLogin,
-            String emailPassword) {
-        super();
-        this.emailFrom = emailFrom;
-        this.emailTo = emailTo;
-        this.emailLogin = emailLogin;
-        this.emailPassword = emailPassword;
-    }
-
-    public void send(String subject, String text) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(emailLogin,
-                                emailPassword);
-                    }
-                });
-
-        try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(emailFrom));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(emailTo));
-            message.setSubject(subject);
-            message.setText(text);
-
-            Transport.send(message);
-
-            LOG.debug("Email to " + emailTo + " send.");
-
-        } catch (MessagingException e) {
-            throw new EmailSenderException(e.getMessage());
-        }
-    }
+    private String [] emailsTo;
+    
+	public void setMailSender(MailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+	
+	public void setEmailFrom(String emailFrom){
+		this.emailFrom = emailFrom;
+	}
+	
+	public void setEmailsTo(String [] emailsTo){
+		this.emailsTo = emailsTo;
+	}
+ 
+	/**
+	 * Send email
+	 * @param subject
+	 * @param msg
+	 */
+	public void send(String subject, String msg) {
+ 
+		SimpleMailMessage message = new SimpleMailMessage();
+ 
+		message.setFrom(emailFrom);
+		message.setTo(emailsTo);
+		message.setSubject(subject);
+		message.setText(msg);
+		mailSender.send(message);
+		LOG.debug("Email to " + emailsTo.toString() + " sent.");
+	}
 }
--- src/test/java/com/epam/jiranotificator/service/EmailSenderTest.java
+++ src/test/java/com/epam/jiranotificator/service/EmailSenderTest.java
@@ -4,12 +4,17 @@
 package com.epam.jiranotificator.service;
 
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 
 import com.epam.jiranotificator.exception.EmailSenderException;
 import com.epam.jiranotificator.service.impl.EmailSender;
@@ -21,21 +26,37 @@ import com.epam.jiranotificator.service.impl.EmailSender;
 @RunWith(MockitoJUnitRunner.class)
 public class EmailSenderTest {
     
-    private static final String FROM = "from@gmail.com";
-    private static final String TO = "to@gmail.com";
-    private static final String LOGIN = "login";
-    private static final String PASSWORD = "password";
     private static final String SUBJECT = "subject";
     private static final String TEXT = "text";
+    private static final String FROM = "test";
+    private static final String [] TO = new String[]{"test", "test"};
+    
+    private SimpleMailMessage message;
+    
+    @Mock
+    private MailSender mailSender;
     
     @Spy
     @InjectMocks
-    private EmailSender emailSender = new EmailSender(FROM, TO, LOGIN, PASSWORD);
+    private EmailSender emailSender = new EmailSender();
 
-//    @Test
-//    public void shouldSuccessfulSendMessage() {
-//        emailSender.send(SUBJECT, TEXT);
-//    }
+    @Before
+    public void setup(){
+    	emailSender.setMailSender(mailSender);
+    	emailSender.setEmailFrom(FROM);
+    	emailSender.setEmailsTo(TO);
+    	message = new SimpleMailMessage();
+    	message.setFrom(FROM);
+    	message.setTo(TO);
+    	message.setSubject(SUBJECT);
+    	message.setText(TEXT);
+    }
+    
+    @Test
+    public void shouldSuccessfulSendMessage() {
+        emailSender.send(SUBJECT, TEXT);
+        verify(mailSender).send(message);
+    }
 
     @Test(expected = EmailSenderException.class)
     public void shouldThrowException() throws Exception {
